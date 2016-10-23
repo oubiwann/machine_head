13713f7b01439c66379eb61acb1a3d6726829726
Duncan McGreggor <oubiwann@gmail.com>
2016-10-23 15:50:09 -0500

    Moved maven repos into publish profile.
    Also bumped beta version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/machine_head "1.0.0-beta10-SNAPSHOT"
+(defproject clojurewerkz/machine_head "1.0.0-beta11-SNAPSHOT"
   :description "Clojure MQTT client"
   :dependencies [[org.clojure/clojure          "1.8.0"]
                  [org.eclipse.paho/org.eclipse.paho.client.mqttv3 "1.0.2"]
@@ -8,17 +8,18 @@
              :dev {:resource-paths ["test/resources"]
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
-                           :output-dir "doc/api"}}}
+                           :output-dir "doc/api"}}
+             :publish {
+                   :repositories {"eclipse-paho" {:url "https://repo.eclipse.org/content/repositories/paho-releases/"
+                                                  :snapshots false
+                                                  :releases {:checksum :fail}}
+                                  "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                                              :snapshots false
+                                              :releases {:checksum :fail :update :always}}
+                                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                                        :snapshots true
+                                                        :releases {:checksum :fail :update :always}}}}}
   :aliases {"all" ["with-profile" "dev:dev,1.7:dev,master"]}
-  :repositories {"eclipse-paho" {:url "https://repo.eclipse.org/content/repositories/paho-releases/"
-                                 :snapshots false
-                                 :releases {:checksum :fail}}
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
-                             :snapshots false
-                             :releases {:checksum :fail :update :always}}
-                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
-                                       :snapshots true
-                                       :releases {:checksum :fail :update :always}}}
   :javac-options      ["-target" "1.6" "-source" "1.6"]
   :jvm-opts           ["-Dfile.encoding=utf-8"]
   :source-paths       ["src/clojure"]
